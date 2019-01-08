d7255e4bacf743f159f21de14d3c94adb64368fc
Blake Buck <blakemanbuck@gmail.com>
2019-01-08 15:55:07 -0600

    Complete problems

--- practice.js
+++ practice.js
@@ -28,7 +28,9 @@
 */
 
 // Code Here 
-
+function first(arr, callback){
+	callback(arr[0]);
+}
 // Do not edit the code below.
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 
@@ -48,6 +50,9 @@ first(names, function(firstName){
 */
 
 //Code Here
+function last(arr, callback){
+	callback(arr[arr.length-1]);
+}
 
 // Do not edit the code below.
 last(names, function(lastName){
@@ -66,7 +71,9 @@ last(names, function(lastName){
 */
 
 //Code Here
-
+function multiply(num1, num2, callback){
+	callback(num1*num2);
+}
 // Do not edit the code below.
 multiply(4, 3, function(answer){
   console.log('The answer is ' + answer); //should console.log 12
@@ -85,7 +92,10 @@ multiply(4, 3, function(answer){
 */
 
 //Code Here 
-
+function contains(arr, name, callback){
+	if(arr.indexOf(name)>=0)callback(true);
+	else callback(false);
+}
 // Do not edit the code below.
 contains(names, 'Colt', function(result){
   if(result === true){
@@ -106,7 +116,20 @@ contains(names, 'Colt', function(result){
 */
 
 //Code Here
-
+function uniq(arr, callback){
+	
+	
+	for(var j=0; j<arr.length-1; j++){
+		for(var i=j+1; i<arr.length; i++){
+			if(arr[j] === arr[i]){
+				arr.splice(i,1);
+				i=j+1;
+			}
+		}
+	}
+	
+	callback(arr);
+}
 // Do not edit the code below.
 uniq(names, function(uniqArr){
   console.log('The new names array with all the duplicate items removed is ', uniqArr);
@@ -123,7 +146,11 @@ uniq(names, function(uniqArr){
 */
 
 //Code Here 
-
+function each(names, callback){
+	for(var i=0; i<names.length; i++){
+		callback(names[i],i);
+	}
+}
 // Do not edit the code below.
 each(names, function(item, indice){
   console.log('The item in the ' + indice + ' position is ' + item)
@@ -140,6 +167,11 @@ each(names, function(item, indice){
 */
 
 // Code here
+function getUserById(users,id,callback){
+	for(var i=0; i<users.length; i++){
+		if(users[i].id === id)callback(users[i]);
+	}
+}
 
 // Do not edit the code below.
 var users = [
